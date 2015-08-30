afb31019c18338f132a95f26c61dd010e1b97131
morikawa_kaori <kaori.morikawa0507@gmail.com>
2015-08-30 20:01:22 +0900

    add lessons

---
+++ lessons/Driver.scala
@@ -0,0 +1,23 @@
+object Driver {
+	def main(args: Array[String]): Unit = {
+
+		val double = (value: Int) => value * 2
+		println(double(10))
+
+		myLoop(10, double)
+		myLoop(10, (value: Int) => value * 3)
+		myLoop(10, nTimes(5))
+	}
+
+	def myLoop(max:Int, func: (Int) => Int) = {
+		for(i <- 1 to max) {
+			print(func(i) + " ")
+			println
+		}
+	}
+
+	def nTimes(times: Int): (Int) => Int = {
+		(value: Int) => times * value
+	}
+
+}
