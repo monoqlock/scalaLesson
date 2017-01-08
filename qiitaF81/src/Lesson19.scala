013db5cf8d2be4ad6b7d8c5e5ec297fde79a0b44
morikawa_kaori <kaori.morikawa0507@gmail.com>
2017-01-08 16:20:35 +0900

    add Lesson19

---
+++ qiitaF81/src/Lesson19.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by morikawakaori on 2017/01/08.
+  */
+object Lesson19 {
+
+  def main(args: Array[String]): Unit = {
+
+    def addCurried(x: Int)(y: Int)(z: Int) = x + y + z
+    println(addCurried(2)(3)(4)) // 9
+
+    def addPartical(x: Int, y: Int, z: Int) = x + y + z
+    val addParticalWithX = addPartical(_:Int, _: Int, 5)
+    println(addParticalWithX(1, 2)) // 8
+
+  }
+
+}
